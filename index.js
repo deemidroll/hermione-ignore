0c6b9632ea6094069dd2a65405ea9775f49dd0a4
Denis Avdeev <den-a1997@yandex.ru>
2018-08-17 18:43:16 +0300

    Удаляем из глобального игнора явно переданные исключения

--- index.js
+++ index.js
@@ -17,13 +17,23 @@ module.exports = (hermione, opts = {}) => {
 
         browser.addCommand('assertView', (name, selector, options = {}) => {
             options.excludeElements = normalize(options.excludeElements);
-
+            
             // Merge global and local selectors without excluded selectors.
-            ['ignoreElements', 'hideElements', 'invisibleElements'].forEach(prop => {
+            ['ignoreElements', 'hideElements', 'invisibleElements'].forEach((prop, index, arr) => {
                 options[prop] = merge(
-                    globalIgnore[prop],
+                    globalIgnore[`${prop}Elements`],
                     normalize(options[prop]),
-                    options.excludeElements
+                    // Удалить из глобального игнора:
+                    // - явно переданные исключения
+                    // - селектор, который скриншотится
+                    // - другие виды игнора
+                    [].concat(
+                        options.excludeElements,
+                        options.selector,
+                        arr.reduce((acc, p) => {
+                            return p !== prop ? acc.concat(options[p]) : acc;
+                        }, [])
+                    )
                 );
             });
 
